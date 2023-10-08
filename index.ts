af16c43d3f99badb41de66149e9ba26eb3c1db6b
Michael Tran <48533120+Mst3kMantra@users.noreply.github.com>
2023-10-08 16:34:56 -0500

    finished tutorial

--- index.ts
+++ index.ts
@@ -1,10 +1,10 @@
 require('dotenv').config();
 
-const express = require('express');
-const mongoose = require('mongoose');
+export const express = require('express');
+export const mongoose = require('mongoose');
 const mongoString = process.env.DATABASE_URL;
 
-const routes = require('./routes/routes');
+export const routes = require('./routes/routes');
 
 mongoose.connect(mongoString);
 
@@ -20,9 +20,9 @@ database.once('connected', () => {
 
 const app = express();
 
-app.use('/api', routes)
-
 app.use(express.json());
+
+app.use('/api', routes)
 
 app.listen(3000, () => {
     console.log(`Server Started at ${3000}`)
